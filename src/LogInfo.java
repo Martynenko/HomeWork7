e995e1b19a5a0e3149f8e66b8972b6d792992db9
Мартыненко Артем Сергеевич <artem.martynenko@privatbank.ua>
2021-07-29 19:39:50 +0300

    first commit

--- src/LogInfo.java
+++ src/LogInfo.java
@@ -2,11 +2,10 @@ import java.util.logging.Logger;
 
 public class LogInfo {
 
-        private final static Logger logger = Logger.getLogger("");
+    private final static Logger logger = Logger.getLogger("");
 
     public void getlogInfo() {
-
-                logger.info("лог, который записывает важные действия в приложении. Это не ошибки, это не предостережение, это ожидаемые действия системы;");
+        logger.info("лог, который записывает важные действия в приложении. Это не ошибки, это не предостережение, это ожидаемые действия системы;");
     }
 
 }
